8fa996530fbec45f2b1ed489242e214737ddc0a7
GemaLuo <gemaluowenjun@gmail.com>
2023-06-04 16:20:19 +0800

    setup: setup firebase

---
+++ auviConverter/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDu1E31ordcF6Ky9Af3pUNqcUOXVk9s1nY",
+  authDomain: "converter-you2be.firebaseapp.com",
+  projectId: "converter-you2be",
+  storageBucket: "converter-you2be.appspot.com",
+  messagingSenderId: "137423813232",
+  appId: "1:137423813232:web:3c7177e67f01a8bde4d2b5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebase;
